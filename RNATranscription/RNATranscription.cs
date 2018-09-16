205532c21e333acb21854c90151338696a116216
Adarshi Sinha <adarshisinha@gmail.com>
2018-09-16 19:59:06 +0530

    Ass3

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -1,27 +1,36 @@
 ﻿using System;
+using System.Text;
 
 namespace RNATranscriptionModule
 {
     public static class RnaTranscription
     {
+        private const string V = "C";
+        private const char V1 = 'G';
+        private const char V2 = 'A';
+        private const char V3 = 'U';
+
         public static string ToRna(string nucleotide)
         {
-            string str=null;
+            
+            StringBuilder str=new StringBuilder(nucleotide.Length);
+            //replacing dna with rna by replacing each element with rna element
             foreach(char i in nucleotide)
             {
                 if(i=='G')
-                str+='C';
+                str.Append("C");
                 else if(i=='C')
-                str+='G';
+                str.Append("G");
                 else if(i=='T')
-                str+='A';
+                str.Append("A");
                 else if(i=='A')
-                str+='U';
+                str.Append("U");
                 else
                 throw new ArgumentException("String should not contain");
             }
            // str+="\0";
-            return str;
+           string st= str.ToString();
+            return st;
           // throw new NotImplementedException("You need to implement this function.");
         }
     }
